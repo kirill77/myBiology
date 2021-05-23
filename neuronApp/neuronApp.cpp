e97551728954c773a4f3e003e3563a783aa1e372
kirill77 <kirill77>
2021-05-23 14:26:48 -0700

    refactoring, added text rendering functions

--- neuronApp/neuronApp.cpp
+++ neuronApp/neuronApp.cpp
@@ -6,6 +6,8 @@
 #include <easy3d/fileio/resources.h>
 #include <easy3d/util/logging.h>
 #include <easy3d/util/file_system.h>
+#include <easy3d/renderer/text_renderer.h>
+#include <3rd_party/glfw/include/GLFW/glfw3.h>	// for the KEYs
 
 #include "neuron.h"
 
@@ -89,11 +91,80 @@ private:
         Viewer::pre_draw();
         m_bIsFirstDraw = false;
     }
+    virtual void draw() const override
+    {
+        Viewer::draw();
+
+        const float font_size = 28.0f + font_size_delta_;
+        float x = 50.0f;
+        float y = 80.0f;
+
+        const int num_fonts = texter_->num_fonts();
+        const float font_height = texter_->font_height(font_size);
+
+        texter_->draw("hello, world",
+            x * dpi_scaling(), y * dpi_scaling(), font_size, TextRenderer::Align(alignment_), 0, vec3(0, 0, 0),
+            line_spacing_, upper_left_);
+    }
+    bool key_press_event(int key, int modifiers) override
+    {
+        float spacing = 0;
+        bool kerning = false;
+        switch (key)
+        {
+        case GLFW_KEY_MINUS:
+                font_size_delta_ = std::max(font_size_delta_ - 1.0f, -20.0f);
+                break;
+        case GLFW_KEY_EQUAL:
+                font_size_delta_ = std::min(font_size_delta_ + 1.0f, 250.0f);
+                break;
+        case GLFW_KEY_COMMA:
+                spacing = texter_->character_spacing();
+                texter_->set_character_spacing(std::max(spacing - 0.5f, 0.0f));
+                break;
+        case GLFW_KEY_PERIOD:
+                spacing = texter_->character_spacing();
+                texter_->set_character_spacing(std::min(spacing + 0.5f, 50.0f));
+                break;
+        case GLFW_KEY_DOWN:
+                line_spacing_ = std::max(line_spacing_ - 0.1f, -1.0f);
+                break;
+        case GLFW_KEY_UP:
+                line_spacing_ = std::min(line_spacing_ + 0.1f, 2.0f);
+                break;
+        case GLFW_KEY_L:
+                alignment_ = TextRenderer::ALIGN_LEFT;
+                break;
+        case GLFW_KEY_C:
+                alignment_ = TextRenderer::ALIGN_CENTER;
+                break;
+        case GLFW_KEY_R:
+                alignment_ = TextRenderer::ALIGN_RIGHT;
+                break;
+        case GLFW_KEY_O:
+                upper_left_ = !upper_left_;
+                break;
+        case GLFW_KEY_SPACE:
+                kerning = texter_->kerning();
+                texter_->set_kerning(!kerning);
+                break;
+        default:
+                return Viewer::key_press_event(key, modifiers);
+        }
+        return true;
+    }
+
     bool m_bIsFirstDraw = true;
     std::chrono::high_resolution_clock::time_point m_prevDrawTS;
     MyPointsDrawable *m_pKDrawable = nullptr, *m_pNaDrawable = nullptr; // pointers owned by viewer
     Neuron<T> m_neuron;
     Viewer* m_pViewer = nullptr;
+
+    // for text rendering:
+    float font_size_delta_ = -20;
+    float line_spacing_ = 0;
+    int alignment_ = TextRenderer::ALIGN_CENTER;
+    bool upper_left_ = true;
 };
 
 int main(int argc, char** argv)
