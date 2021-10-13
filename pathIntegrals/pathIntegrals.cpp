ae3959fc8e9f2bce75f88f4a00251680ab9b99dd
kirill77 <kirill77>
2021-10-12 19:38:15 -0700

    pathIntegrals - i compute stationary action for single electron going in circular orbit around proton

--- pathIntegrals/pathIntegrals.cpp
+++ pathIntegrals/pathIntegrals.cpp
@@ -1,15 +1,63 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <basics/myunits.h>
+#include <basics/gradientDescent.h>
+
+typedef double T;
+
+MyUnits<T> computeAction(MyUnits<T> fOrbitRadius, MyUnits<T> fSpeed)
+{
+    MyUnits<T> fKineticEnergy = MyUnits<T>::electronMass() * fSpeed * fSpeed / 2;
+    MyUnits<T> fPotentialEnergy = chargePotentialEnergy<T>(MyUnits<T>::electronCharge(), MyUnits<T>::electronCharge(), fOrbitRadius);
+    MyUnits<T> fLagrangian = fKineticEnergy - fPotentialEnergy;
+    MyUnits<T> fTime = fOrbitRadius * 2 * M_PI / fSpeed;
+    MyUnits<T> fAction = fTime * fLagrangian;
+    MyUnits<T> fDividedAction = fAction / MY_PLANCK_CONSTANT;
+    return fDividedAction;
+}
+
+struct OrbitFunctor
+{
+    OrbitFunctor(MyUnits<T> fSpeed) : m_fSpeed(fSpeed) { }
+    double operator ()(double fOrbitRadius)
+    {
+        return computeAction(MyUnits<T>(fOrbitRadius), m_fSpeed).m_value;
+    }
+private:
+    MyUnits<T> m_fSpeed;
+};
+struct SpeedFunctor
+{
+    SpeedFunctor(MyUnits<T> fOrbitRadius) : m_fOrbitRadius(fOrbitRadius) { }
+    double operator ()(double fSpeed)
+    {
+        return computeAction(m_fOrbitRadius, MyUnits<T>(fSpeed)).m_value;
+    }
+private:
+    MyUnits<T> m_fOrbitRadius;
+};
+
+struct XCubeFunctor
+{
+    double operator ()(double fX)
+    {
+        return fX * fX * fX;
+    }
+};
 
 int main()
 {
     MyUnitsTest::test();
 
-#if 0
-    // say electron wizzes around hydrogen atom in circular orbit
-    MyUnits<double> fOrbitRadius = MyUnits<double>::angstrom();
-    MyUnits<double> fSpeed = 2000000 * MyUnits<double>::meter() / MyUnits<double>::second();
+    //double f = searchStationaryPoint(XCubeFunctor(), 10, 128);
 
-    MyUnits<T> fKineticEnergy =  * fSpeed * fSpeed / 2;
-    MyUnits<T> fPotentialEnergy = MyUnits<T>::dalton() * 
-#endif
+    // say electron whizzes around hydrogen atom in circular orbit. by varying the orbit radius and the electron speed try to find the
+    // parameters with stationary action
+    MyUnits<T> fCurOrbitRadius = MyUnits<T>::angstrom();
+    MyUnits<T> fCurSpeed = MyUnits<double>::meter() * 2000000 / MyUnits<double>::second();
+    for (NvU32 u = 0; u < 128; ++u)
+    {
+        fCurSpeed = MyUnits<T>(searchStationaryPoint(SpeedFunctor(fCurOrbitRadius), fCurSpeed.m_value, 128));
+        fCurOrbitRadius = MyUnits<T>(searchStationaryPoint<OrbitFunctor>(OrbitFunctor(fCurSpeed), fCurOrbitRadius.m_value, 128));
+    }
 }
